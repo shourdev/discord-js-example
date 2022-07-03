58e1a2c7710706c082174ffc9326e4e3a5a700fd
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-03 10:59:24 +0530

    Made ping cmd

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 client.on('ready', () =>{
 	client.user.setStatus('Discord.js Example!')
 	console.log('Connected!')
+client.on('message', message => {
+  if (message.content === 'ping') {  
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  }
+});
 
 })
 client.login('')
