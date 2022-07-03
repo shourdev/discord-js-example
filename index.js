6ea8d8c07728e3bed1d77b573b7a3344f7476150
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-03 10:09:15 +0530

    Removed the token from the code and improved it a bit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
 
 client.on('ready', () =>{
 	client.user.setStatus('Discord.js Example!')
 	console.log('Connected!')
-})
 
+})
 client.login('')
